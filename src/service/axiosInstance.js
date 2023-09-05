1f0ff967391fef2b8ef5a4ea78bbff65c981a46a
Erwin Ramadhan <erwinramadhan@isws-MacBook-Air.local>
2023-09-05 23:28:36 +0700

    adding cors

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://103.175.219.0:1337/api'
+    baseURL: 'http://103.175.219.0:1337/api',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
+      },
 });
 
 export default axiosInstance
